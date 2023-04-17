b9fa660bb9ec1bcdc2def95a1db623b419f150e9
pujaKundu <iampujakundu@gmail.com>
2023-04-18 00:13:28 +0600

    reg styles

--- src/components/student/StudentRegistration.js
+++ src/components/student/StudentRegistration.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRegisterMutation } from "../../features/auth/authApi";
-import logo from "../../image/learningportal.svg";
+import logo from "../../image/code.png";
 import Error from "../ui/Error";
 
 const StudentRegistration = () => {
@@ -46,105 +46,107 @@ const StudentRegistration = () => {
   };
 
   return (
-    <div>
-      <section className="py-6 bg-primary h-screen grid place-items-center">
-        <div className="mx-auto max-w-md px-5 lg:px-0">
-          <div>
-            <img className="h-12 mx-auto" src={logo} alt="logo" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
-              Create Your New Account
-            </h2>
+    <section className="py-6 bg-primary h-screen grid place-items-center">
+      <div className="mx-auto max-w-md px-5 lg:px-0">
+        <div className="flex flex-col items-center justify-start">
+          <div className="flex items-center justify-start">
+            <img src={logo} alt="logo" width={35} height={35} />
+            <p className="ml-2 text-2xl font-semibold ">
+              <span className="text-cyan-300">The Dev</span> Spot
+            </p>
           </div>
-          <form
-            className="mt-8 space-y-6"
-            action="#"
-            method="POST"
-            onSubmit={handleSubmit}
-          >
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="name" className="sr-only">
-                  Name
-                </label>
-                <input
-                  id="name"
-                  name="name"
-                  type="name"
-                  autoComplete="name"
-                  required
-                  className="login-input rounded-t-md bg-slate-900"
-                  placeholder="Student Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="email-address " className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="login-input bg-slate-900"
-                  placeholder="Email address"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="login-input bg-slate-900"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="confirm-password" className="sr-only">
-                  Confirm Password
-                </label>
-                <input
-                  id="confirm-password"
-                  name="confirm-password"
-                  type="password"
-                  autoComplete="confirm-password"
-                  required
-                  className="login-input rounded-b-md bg-slate-900"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent
-                 text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none
-                  focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
-                disabled={isLoading}
-              >
-                Create Account
-              </button>
-            </div>
-            {error !== "" && <Error message={error} />}
-          </form>
+          <h2 className="mt-6 text-center text-xl font-semibold text-slate-100">
+            <p className="text-xl text-violet-400">Create new account</p>
+          </h2>
         </div>
-      </section>
-    </div>
+        <form
+          className="mt-8 space-y-6 w-[470px]"
+          action="#"
+          method="POST"
+          onSubmit={handleSubmit}
+        >
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="login-input border-0 border-b mb-3 text-slate-200 bg-transparent"
+                placeholder="Student Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email-address " className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="login-input bg-transparent border-0 border-b mb-3 text-slate-200"
+                placeholder="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="login-input bg-transparent border-0 border-b mb-3 text-slate-200"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="confirm-password" className="sr-only">
+                Confirm Password
+              </label>
+              <input
+                id="confirm-password"
+                name="confirm-password"
+                type="password"
+                autoComplete="confirm-password"
+                required
+                className="login-input bg-transparent border-0 border-b mb-3 text-slate-200"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent
+                  font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none
+                  focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+              disabled={isLoading}
+            >
+              Create Account
+            </button>
+          </div>
+          {error !== "" && <Error message={error} />}
+        </form>
+      </div>
+    </section>
   );
 };
 
